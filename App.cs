c63a1061efc1121e7db5e0176046803b9c0a573d
Henning Wikene <d450hewi@systemair.se>
2025-02-22 17:31:39 +0100

    Initial commit - Added RunDynamoScript project

---
+++ App.cs
@@ -0,0 +1,73 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace YourNamespace
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "YourTabNameHere"; // Set the name of the tab in the Revit ribbon
+
+            // Attempt to create a new tab in the Revit ribbon
+            try
+            {
+                application.CreateRibbonTab(tabName);
+            }
+            catch (Exception)
+            {
+                // If the tab already exists, do nothing
+            }
+
+            // Create a new section in the tab for your tools
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "YourPanelNameHere");
+
+            // Define a new button to execute the Dynamo script
+            PushButtonData buttonData = new PushButtonData(
+                "YourButtonNameHere", // Unique identifier for the button
+                "YourButtonLabelHere", // Text displayed on the button
+                Assembly.GetExecutingAssembly().Location,
+                "YourNamespace.YourCommandHere"); // Full namespace and class of the command
+
+            // Add the button to the Ribbon Panel
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            // Set the icon and tooltip for the button
+            pushButton.LargeImage = PngImageSource("Resources/YourIconHere.png"); // Path to your image resource
+            pushButton.ToolTip = "YourTooltipHere: \n\n" +
+                                 "- Description of feature 1.\n" +
+                                 "- Description of feature 2.\n" +
+                                 "- Description of feature 3.\n" +
+                                 "- Description of feature 4.";
+
+            // Add a help page link if needed
+            ContextualHelp help = new ContextualHelp(ContextualHelpType.Url, "YourHelpFilePathHere"); // Set the URL or file path to documentation
+            pushButton.SetContextualHelp(help);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // No special actions required on shutdown
+            return Result.Succeeded;
+        }
+
+        private BitmapImage PngImageSource(string embeddedPath)
+        {
+            // Helper method to load an image for the button
+            try
+            {
+                Uri uri = new Uri($"pack://application:,,,/YourAssemblyName;component/{embeddedPath}", UriKind.Absolute);
+                return new BitmapImage(uri);
+            }
+            catch
+            {
+                // Return null if the image cannot be loaded
+                return null;
+            }
+        }
+    }
+}
---
+++ RunDynamoScript.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Dynamo.Applications.Models; // Ensure this is the correct namespace for your version
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms; // For UI components
+using Dynamo.Applications;
+using Autodesk.Revit.Attributes;
+
+namespace YourNamespace
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class RunDynamoScript : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get application and document objects and start transaction
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Define the path to the Dynamo script (update this in configuration)
+            string dynamoScriptPath = "YourDynamoScriptPathHere";
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData();
+            dynamoRevitCommandData.Application = commandData.Application;
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                { Dynamo.Applications.JournalKeys.ShowUiKey, false.ToString() }, // Hide Dynamo UI at runtime
+                { Dynamo.Applications.JournalKeys.AutomationModeKey, true.ToString() }, // Run in automation mode
+                { Dynamo.Applications.JournalKeys.DynPathKey, dynamoScriptPath }, // Set path to Dynamo script
+                { Dynamo.Applications.JournalKeys.DynPathExecuteKey, true.ToString() }, // Execute the script
+                { Dynamo.Applications.JournalKeys.ForceManualRunKey, false.ToString() }, // Do not force manual run mode
+                { Dynamo.Applications.JournalKeys.ModelShutDownKey, true.ToString() }, // Shut down model after execution
+                { Dynamo.Applications.JournalKeys.ModelNodesInfo, false.ToString() } // Disable node information logging
+            };
+
+            dynamoRevitCommandData.JournalData = journalData;
+            Result externalCommandResult = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+            return externalCommandResult;
+        }
+    }
+}
